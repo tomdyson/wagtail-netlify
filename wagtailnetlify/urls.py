2c62bad7ee4b38c1c0254961eb153a9dc96afcc8
Tom Dyson <tom.dyson@roh.org.uk>
2022-05-04 14:36:04 +0100

    Update for Django 4 compatibility

--- wagtailnetlify/urls.py
+++ wagtailnetlify/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path
 from wagtailnetlify.views import redirects
 
-urlpatterns = [url(r"^redirects$", redirects, name="redirect_builder")]
+urlpatterns = [re_path(r"^redirects$", redirects, name="redirect_builder")]
